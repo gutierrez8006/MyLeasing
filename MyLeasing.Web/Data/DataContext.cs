601348dbab8e0d0590863adedd25267346a08523
Juan Carlos Gutierrez <gutierrez.8006@gmail.com>
2019-11-17 21:09:06 -0500

    PARTE 07

--- MyLeasing.Web/Data/DataContext.cs
+++ MyLeasing.Web/Data/DataContext.cs
@@ -1,13 +1,10 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using MyLeasing.Web.Data.Entities;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace MyLeasing.Web.Data
 {
-    public class DataContext : DbContext
+    public class DataContext : IdentityDbContext<User>
     {
         public DataContext(DbContextOptions<DataContext> options) : base(options)
         {
@@ -17,6 +14,7 @@ namespace MyLeasing.Web.Data
         public DbSet<Contract> Contracts { get; set; }
         public DbSet<Lessee> Lessees { get; set; }
         public DbSet<Owner> Owners { get; set; }
+        public DbSet<Manager> Managers { get; set; }
         public DbSet<Property> Properties { get; set; }
         public DbSet<PropertyImage> PropertyImages { get; set; }
         public DbSet<PropertyType> PropertyTypes { get; set; }
