8c0bf0b5e7a362d89e91ad0580ac7b4b0f866f5c
Juan Carlos Gutierrez <gutierrez.8006@gmail.com>
2019-12-01 17:59:47 -0500

    Implementing login and logout in Web

--- MyLeasing.Web/Helpers/IUserHelper.cs
+++ MyLeasing.Web/Helpers/IUserHelper.cs
@@ -1,8 +1,6 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using MyLeasing.Web.Data.Entities;
-using System;
-using System.Collections.Generic;
-using System.Linq;
+using MyLeasing.Web.Models;
 using System.Threading.Tasks;
 
 namespace MyLeasing.Web.Helpers
@@ -18,5 +16,7 @@ namespace MyLeasing.Web.Helpers
         Task AddUserToRoleAsync(User user, string roleName);
 
         Task<bool> IsUserInRoleAsync(User user, string roleName);
+        Task<SignInResult> LoginAsync(LoginViewModel model);
+        Task LogoutAsync();
     }
 }
